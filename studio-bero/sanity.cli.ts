e1706cbe38e9634117555858d7c5202c052098a9
Monika Szumna <monika.szumna@auctane.com>
2026-03-10 17:06:35 +0100

    feat(cms): deploy sanity studio

--- studio-bero/sanity.cli.ts
+++ studio-bero/sanity.cli.ts
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: process.env.SANITY_STUDIO_APP_ID,
   },
 });
