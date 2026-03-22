77f40afb17acf06a800bd85176b67f9fdacb316d
monaboiste <monikaszumna4@gmail.com>
2026-03-23 00:17:46 +0100

    chore: ignore false-positive short import suggestion

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 
 import { getViteConfig } from "astro/config";
+// noinspection ES6PreferShortImport
 import { svgMockPlugin } from "./src/test/plugins/svg-mock/svg-mock.ts";
 
 export default getViteConfig({
