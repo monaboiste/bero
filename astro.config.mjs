bef3aed76e0df6c013c06e3bb65a53b9cfa5ab3d
monaboiste <monikaszumna4@gmail.com>
2026-03-23 16:40:07 +0100

    feat(seo): add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import { defaultLang, locales } from "./src/i18n/locale";
@@ -8,6 +9,17 @@ import { defaultLang, locales } from "./src/i18n/locale";
 // https://astro.build/config
 export default defineConfig({
   site: "https://studio-bero.com",
+
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("/404"),
+      i18n: {
+        defaultLocale: defaultLang,
+        locales: Object.fromEntries(locales.map((lang) => [lang, lang])),
+      },
+    }),
+  ],
+
   i18n: {
     defaultLocale: defaultLang,
     locales: [...locales],
