56e91deac6e18455a46e5eb5e84d0543fa505f30
monaboiste <monikaszumna4@gmail.com>
2026-03-23 13:37:44 +0100

    feat(seo): add canonical site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defaultLang, locales } from "./src/i18n/locale";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://studio-bero.com",
   i18n: {
     defaultLocale: defaultLang,
     locales: [...locales],
