710209e6f375da104fb8dd58595414c5d1bcf786
monaboiste <monikaszumna4@gmail.com>
2026-05-21 21:13:45 +0200

    chore(test): remove unused mock svg plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,8 @@
 /// <reference types="vitest/config" />
 
 import { getViteConfig } from "astro/config";
-// noinspection ES6PreferShortImport
-import { svgMockPlugin } from "./src/test/plugins/svg-mock/svg-mock.ts";
 
 export default getViteConfig({
-  plugins: [svgMockPlugin()],
   test: {
     environment: "happy-dom",
     globals: true,
